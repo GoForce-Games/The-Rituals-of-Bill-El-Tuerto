221edd589ba1a49ee7a6bbf3905ab19b8aa8eaac
errico6 <125646224+errico6@users.noreply.github.com>
2025-05-27 12:11:53 +0200

    Ajustes de dimensiones

--- Assets/Scripts/Movimiento Secuencial.cs
+++ Assets/Scripts/Movimiento Secuencial.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class MovimientoRelativo : MonoBehaviour
 {
-    public Vector3 desplazamiento1 = new Vector3(3, 0, 0);  // Por ejemplo: +3 en X
-    public Vector3 desplazamiento2 = new Vector3(0, 0, 5);  // Por ejemplo: +5 en Z
+    public Vector3 desplazamiento1 = new Vector3(, 0, 0); 
+    public Vector3 desplazamiento2 = new Vector3(0, 0, 0); 
 
     void Start()
     {
@@ -14,14 +14,11 @@ public class MovimientoRelativo : MonoBehaviour
 
     System.Collections.IEnumerator MoverSecuencial()
     {
-        // Guarda la posición inicial
         Vector3 inicio = transform.position;
 
-        // Calcula destino1 relativo
         Vector3 destino1 = inicio + desplazamiento1;
         yield return StartCoroutine(MoverAObjetivo(destino1, 1f));
 
-        // Calcula destino2 relativo desde destino1
         Vector3 destino2 = destino1 + desplazamiento2;
         yield return StartCoroutine(MoverAObjetivo(destino2, 1f));
     }
