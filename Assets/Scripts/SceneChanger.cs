33752e7631afcff5af54820be72cdf31f49fa620
Roger Puchol Celma <49079760+Ropuce@users.noreply.github.com>
2025-05-29 03:59:55 +0200

    Added game start button to main menu

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -1,18 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class SceneSwapper : MonoBehaviour
+public class SceneChanger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public static void ChangeScene(string sceneName) => SceneManager.LoadScene(sceneName);
 }
