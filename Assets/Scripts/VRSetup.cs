7891efec52823fedff11f8e08154842936707fd1
Roger Puchol Celma <49079760+Ropuce@users.noreply.github.com>
2025-06-02 15:34:45 +0200

    Disabled locomotion in main menu
    Fixed sit/stand toggle not initializing to the correct value

--- Assets/Scripts/VRSetup.cs
+++ Assets/Scripts/VRSetup.cs
@@ -1,15 +1,23 @@
 using Unity.XR.CoreUtils;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VRSetup : MonoBehaviour
 {
+    [SerializeField] private Toggle sitStandToggle;
+    
     // Start is called before the first frame update
     void Start()
     {
-        var isSitting = PlayerPrefs.GetInt("Sitting", 0);
+        bool isSitting = PlayerPrefs.GetInt("Sitting", 0) > 0;
         var xrOrigin = GetComponent<XROrigin>();
         xrOrigin.RequestedTrackingOriginMode =
-            (isSitting > 0 ? XROrigin.TrackingOriginMode.Device : XROrigin.TrackingOriginMode.Floor);
+            (isSitting ? XROrigin.TrackingOriginMode.Device : XROrigin.TrackingOriginMode.Floor);
+
+        if (sitStandToggle)
+        {
+            sitStandToggle.isOn = isSitting;
+        }
     }
 
     public void OnStandSitToggle(bool isSitting)
